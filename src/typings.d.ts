953bf5745470fc79710b11eeba7d237e375cfe10
NotACat1 <notacat.mail@gmail.com>
2024-03-20 21:48:17 +0300

    feat: добавление возможности работать с SASS таблицами

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -6,6 +6,14 @@ declare module '*.css' {
   export = classNames;
 }
 
+declare module '*.scss' {
+  interface IClassNames {
+    [className: string]: string;
+  }
+  const classNames: IClassNames;
+  export = classNames;
+}
+
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.png';
